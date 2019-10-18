342f3f213830b41dad130a3ebad6cb6cc06dce63
Shakwan Williams <shakwanwilliamsart@gmail.com>
2019-10-17 21:44:26 -0400

    finalized title and menu section. now working on scorlling content section with a statid footer possibly.

--- neocosmarecords/src/App.js
+++ neocosmarecords/src/App.js
@@ -5,27 +5,43 @@ import './App.css';
 function App() {
   return (
     <div className = "container">
-      <div className = "pageTitle">
-        <h1> NEO COSMA RECORDS </h1>
-        <h2>alternative internet label</h2>
-      </div>
-      
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Features</a>
-            <a class="nav-item nav-link" href="#">Pricing</a>
-            <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      <div class = "row">
+        <div class = "col">
+          <div className = "pageTitle">
+            <h1> NEO COSMA RECORDS </h1>
+            <h2>alternative internet label</h2>
+            <p> choose your weapon: </p>
           </div>
+          
+          {/* this can be its own component. */}
+          <ul class="nav justify-content-center">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Music</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Visuals</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Artists</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
         </div>
-      </nav>
-      
+      </div>
+
+      <div className = "row">
+        <h1>content</h1>
+      </div>
     </div>
+    
   );
 }
 
